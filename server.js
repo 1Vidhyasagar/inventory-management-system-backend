c3064e12a86c6ff106fc93277aa3b61d6ef7f4b2
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2026-06-08 10:31:56 +0530

    render deploy error fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
-const Product = require("./models/Product");
+const Product = require("./models/product");
 
 const app = express();
 
