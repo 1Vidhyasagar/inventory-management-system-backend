b1135c107f9c1d4163abc81bd6d80cda59450eca
Vidhyasagar <vidhyasagarmyana9598@gmail.com>
2026-06-08 10:24:14 +0530

    initial backend commit

---
+++ server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const Product = require("./models/Product");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find();
+
+  res.json(products);
+});
+
+app.post("/api/products", async (req, res) => {
+  const product = await Product.create(req.body);
+
+  res.json(product);
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    returnDocument: "after",
+  });
+
+  res.json(product);
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+
+  res.json({
+    message: "Deleted",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server Running");
+});
